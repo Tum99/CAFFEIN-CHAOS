ddac4ed3b346e6eb1b2dc79be2b1d8c36d96b40e
Tum99 <faithtum92@gmail.com>
2026-03-24 23:54:51 +0300

    designs the register/login page

---
+++ app/static/JS/auth.js
@@ -0,0 +1,14 @@
+// Selecting the links instead of the submit buttons
+const loginLink = document.querySelector(".signup-container a");
+const registerLink = document.querySelector(".login-container a");
+const authContainer = document.getElementById('auth-container');
+
+loginLink.addEventListener('click', (e) => {
+    e.preventDefault(); // Stops the page from refreshing/redirecting
+    authContainer.classList.add("right-panel-active");
+});
+
+registerLink.addEventListener('click', (e) => {
+    e.preventDefault(); // Stops the page from refreshing/redirecting
+    authContainer.classList.remove("right-panel-active");
+});
