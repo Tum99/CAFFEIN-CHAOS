d252ba17f933f24821aa543bc51d5ccdb6e4badf
Tum99 <faithtum92@gmail.com>
2026-03-25 21:02:51 +0300

    Designs the LogIn/Sign Up page

--- app/static/JS/auth.js
+++ app/static/JS/auth.js
@@ -2,6 +2,11 @@
 const loginLink = document.querySelector(".signup-container a");
 const registerLink = document.querySelector(".login-container a");
 const authContainer = document.getElementById('auth-container');
+const overlayBtn = document.getElementById('overlayBtn')
+
+overlayBtn.addEventListener('click', () => {
+    authContainer.classList.toggle('right__panel__active')
+})
 
 loginLink.addEventListener('click', (e) => {
     e.preventDefault(); // Stops the page from refreshing/redirecting
